54e53fe6ae04b8615ee6e5600787a670376cfb8d
M3IY0U <timo.nock@stud.h-da.de>
2019-05-22 14:52:36 +0200

    Refactoring

--- php/bestellung.php
+++ php/bestellung.php
@@ -1,22 +1,106 @@
-<!DOCTYPE html>
-<html lang="de">
-  <head>
-    <meta charset="utf-8">
-    <title>Übersicht</title>
-    <link rel="stylesheet" href="../css/styles.css">
-    <script src="js/functions.js"></script>
-    <link rel="shortcut icon" href="../res/favicon.ico" type="image/x-icon">
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-    <!-- (unessescary) stuff
-      <base href="https://www.example.de/">
-      <meta name="robots" content="index,follow">
-      <meta name="description" content="Timo stinkt">
-      <meta name="generator" content="Timo Knock">
-    -->
-  </head>
+<?php	// UTF-8 marker äöüÄÖÜß€
+/**
+ * Class Bestellung for the exercises of the EWA lecture
+ * Demonstrates use of PHP including class and OO.
+ * Implements Zend coding standards.
+ * Generate documentation with Doxygen or phpdoc
+ *
+ * PHP Version 5
+ *
+ * @category File
+ * @package  Pizzaservice
+ * @author   Bernhard Kreling, <b.kreling@fbi.h-da.de>
+ * @author   Ralf Hahn, <ralf.hahn@h-da.de>
+ * @license  http://www.h-da.de  none
+ * @Release  1.2
+ * @link     http://www.fbi.h-da.de
+ */
 
+// to do: change name 'Bestellung' throughout this file
+require_once './Page.php';
+require_once './Offer.php';
 
-  <body>
+/**
+ * This is a template for top level classes, which represent
+ * a complete web page and which are called directly by the user.
+ * Usually there will only be a single instance of such a class.
+ * The name of the template is supposed
+ * to be replaced by the name of the specific HTML page e.g. baker.
+ * The order of methods might correspond to the order of thinking
+ * during implementation.
+ * @author   Bernhard Kreling, <b.kreling@fbi.h-da.de>
+ * @author   Ralf Hahn, <ralf.hahn@h-da.de>
+ */
+class Bestellung extends Page
+{
+    // to do: declare reference variables for members
+    // representing substructures/blocks
+
+    /**
+     * Instantiates members (to be defined above).
+     * Calls the constructor of the parent i.e. page class.
+     * So the database connection is established.
+     *
+     * @return none
+     */
+    protected function __construct()
+    {
+        parent::__construct();
+        // to do: instantiate members representing substructures/blocks
+    }
+
+    /**
+     * Cleans up what ever is needed.
+     * Calls the destructor of the parent i.e. page class.
+     * So the database connection is closed.
+     *
+     * @return none
+     */
+    protected function __destruct()
+    {
+        parent::__destruct();
+    }
+
+    /**
+     * Fetch all data that is necessary for later output.
+     * Data is stored in an easily accessible way e.g. as associative array.
+     *
+     * @return none
+     */
+    protected function getViewData()
+    {
+        $offeritems = $this->_database->query("SELECT * FROM offer");
+        if (!$offeritems)
+            throw new Exception("Query failed:" .$_database->error());
+        $result=[];
+        while($item = $offeritems->fetch_assoc()){
+          #$name = $item['OfferName'];
+          #$path = $item['OfferImgPath'];
+          #$price = $item['OfferPrice'];
+          array_push($result,new Offer($item['OfferName'],$item['OfferImgPath'],$item['OfferPrice']));
+          #$result[] = new Offer($name, $path, $price);
+        }
+        return $result;
+    }
+
+    /**
+     * First the necessary data is fetched and then the HTML is
+     * assembled for output. i.e. the header is generated, the content
+     * of the page ("view") is inserted and -if avaialable- the content of
+     * all views contained is generated.
+     * Finally the footer is added.
+     *
+     * @return none
+     */
+    protected function generateView()
+    {
+        $items = $this->getViewData();
+
+        $this->generatePageHeader('Bestellung');
+        // to do: call generateView() for all
+
+echo <<< header
+
       <div class="header">
         <img src="../res/banner.svg" alt="banner" id="logo">
         <div class="header-right">
@@ -31,41 +115,35 @@
       <div id="main">
         <div class="content">
           <div class="headerimage">
-            <!--banner - image mit text?-->
           </div>
             <h1>Unsere Speisekarte</h1>
-            <div class="item">
-              <div class="text">Großer Döner</div>
-              <div class="thumb"></div>
-              <div class="price">4€</div>
-            </div>
-            <div class="item">
-              <div class="text">Kleiner Döner</div>
-              <div class="thumb"></div>
-              <div class="price">3€</div>
-            </div>
-            <div class="item">
-              <div class="text">Veget. Döner</div>
-              <div class="thumb"></div>
-              <div class="price">3,50€</div>
-            </div>
-            <div class="item">
-              <div class="text">Döner Pizza</div>
-              <div class="thumb"></div>
-              <div class="price">5€</div>
-            </div>
+
+header;
+        foreach ($items as $item){
+            $oname = htmlspecialchars($item->name);
+            $opath = htmlspecialchars($item->path);
+            $oprice = htmlspecialchars($item->price);
+echo <<<item
+                        <div class="item">
+                          <div class="text">$oname</div>
+                          <div class="thumb"></div>
+                          <div class="price">$oprice €</div>
+                        </div>
+item;
+        }
+
+echo <<<form
         </div>
       </div>
-
       <div id="right">
         <div class="warenkorb">
           Ihr Warenkorb
-          <form action="https://echo.fbi.h-da.de/">
+          <form action="Bestellung.php" method="post">
               <label>
                 <select name="Bestellung[]" size="6" tabindex="1" multiple>
-                  <option value="gdoener">Großer Döner</option>
-                  <option value="pizza">Döner Pizza</option>
-                  <option value="vdoener" selected>Veget. Döner</option>
+                  <option value="Großer Döner">Großer Döner</option>
+                  <option value="Borgar">Borgar</option>
+                  <option value="Lamacun" selected>Lamacun</option>
                 </select>
               </label>
               <div class="total">14,30€</div>
@@ -88,9 +166,88 @@
             </ul>
       </div>
 
-      <!-- <div class="cookiebar open">
-        <div class="left">Zur optimalen Nutzererfahrung nutzen wir auf unserer Website Cookies. 🍪 <a href="datenschutz.html">Mehr Informationen</a></div>
-        <div class="right"><button class="cookiebar-close" onclick="cookieClose()">Verstanden!</button></div>
-      </div> -->
-  </body>
-</html>
+form;
+        $this->generatePageFooter();
+    }
+
+    /**
+     * Processes the data that comes via GET or POST i.e. CGI.
+     * If this page is supposed to do something with submitted
+     * data do it here.
+     * If the page contains blocks, delegate processing of the
+	 * respective subsets of data to them.
+     * @return none
+     */
+    protected function processReceivedData()
+    {
+        parent::processReceivedData();
+
+        if (sizeof($_POST) > 0){
+          if(!isset($_POST['Name'])||!isset($_POST['PLZ'])||!isset($_POST['Adresse'])||(sizeof($_POST['Bestellung'])<0)){
+            return;
+          }
+
+          try {
+              $sql = "INSERT INTO `order` (OrderID, Adress, OrderTime) VALUES (NULL, ? , CURRENT_TIMESTAMP)";
+              if($stmt = $this->_database->prepare($sql)){
+                $adr = $this->_database->real_escape_string(htmlspecialchars($_POST['Name'] . " " . $_POST['Adresse'] . " " . $_POST['PLZ']));
+                $stmt->bind_param("s", $adr);
+                $stmt->execute();
+                $oid = $this->_database->insert_id;
+                $_SESSION['oid'] = $oid;
+              }else{
+                echo "something broke.:/<br>";
+              }
+
+              $stmt->close();
+              $sql = "";
+              foreach (($_POST['Bestellung']) as $item){
+                $item = $this->_database->real_escape_string($item);
+                $sql .= htmlspecialchars("INSERT INTO `orderitem` (fOrderID, fOfferID) SELECT $oid, OfferID FROM `offer` WHERE offer.OfferName = '$item'; ");
+              }
+              $this->_database->multi_query($sql);
+
+              header('Location: Bestellung.php');
+          } catch (\Exception $e) {
+            throw $e;
+          }
+        }
+    }
+
+    /**
+     * This main-function has the only purpose to create an instance
+     * of the class and to get all the things going.
+     * I.e. the operations of the class are called to produce
+     * the output of the HTML-file.
+     * The name "main" is no keyword for php. It is just used to
+     * indicate that function as the central starting point.
+     * To make it simpler this is a static function. That is you can simply
+     * call it without first creating an instance of the class.
+     *
+     * @return none
+     */
+    public static function main()
+    {
+        try {
+            session_start();
+            $page = new Bestellung();
+            $page->processReceivedData();
+            $page->generateView();
+        }
+        catch (Exception $e) {
+            header("Content-type: text/plain; charset=UTF-8");
+            echo $e->getMessage();
+        }
+    }
+}
+
+// This call is starting the creation of the page.
+// That is input is processed and output is created.
+Bestellung::main();
+
+// Zend standard does not like closing php-tag!
+// PHP doesn't require the closing tag (it is assumed when the file ends).
+// Not specifying the closing ? >  helps to prevent accidents
+// like additional whitespace which will cause session
+// initialization to fail ("headers already sent").
+//? >
--- php/kunde.php
+++ php/kunde.php
@@ -1,22 +1,138 @@
-<!DOCTYPE html>
-<html lang="de">
-  <head>
-    <meta charset="utf-8">
-    <title>Übersicht</title>
-    <link rel="stylesheet" href="../css/styles.css">
-    <script src="js/functions.js"></script>
-    <link rel="shortcut icon" href="../res/favicon.ico" type="image/x-icon">
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
-    <!-- (unessescary) stuff
-      <base href="https://www.example.de/">
-      <meta name="robots" content="index,follow">
-      <meta name="description" content="Timo stinkt">
-      <meta name="generator" content="Timo Knock">
-    -->
-  </head>
+<?php	// UTF-8 marker äöüÄÖÜß€
+/**
+ * Class Kunde for the exercises of the EWA lecture
+ * Demonstrates use of PHP including class and OO.
+ * Implements Zend coding standards.
+ * Generate documentation with Doxygen or phpdoc
+ *
+ * PHP Version 5
+ *
+ * @category File
+ * @package  Pizzaservice
+ * @author   Bernhard Kreling, <b.kreling@fbi.h-da.de>
+ * @author   Ralf Hahn, <ralf.hahn@h-da.de>
+ * @license  http://www.h-da.de  none
+ * @Release  1.2
+ * @link     http://www.fbi.h-da.de
+ */
+
+// to do: change name 'Kunde' throughout this file
+require_once './Page.php';
+require_once './Order.php';
+
+/**
+ * This is a template for top level classes, which represent
+ * a complete web page and which are called directly by the user.
+ * Usually there will only be a single instance of such a class.
+ * The name of the template is supposed
+ * to be replaced by the name of the specific HTML page e.g. baker.
+ * The order of methods might correspond to the order of thinking
+ * during implementation.
+ * @author   Bernhard Kreling, <b.kreling@fbi.h-da.de>
+ * @author   Ralf Hahn, <ralf.hahn@h-da.de>
+ */
+class Kunde extends Page
+{
+    // to do: declare reference variables for members
+    // representing substructures/blocks
+
+    /**
+     * Instantiates members (to be defined above).
+     * Calls the constructor of the parent i.e. page class.
+     * So the database connection is established.
+     *
+     * @return none
+     */
+    protected function __construct()
+    {
+        parent::__construct();
+        // to do: instantiate members representing substructures/blocks
+    }
+
+    /**
+     * Cleans up what ever is needed.
+     * Calls the destructor of the parent i.e. page class.
+     * So the database connection is closed.
+     *
+     * @return none
+     */
+    protected function __destruct()
+    {
+        parent::__destruct();
+    }
+
+    /**
+     * Fetch all data that is necessary for later output.
+     * Data is stored in an easily accessible way e.g. as associative array.
+     *
+     * @return none
+     */
+    protected function getViewData($oid)
+    {
+      $oid = $this->_database->real_escape_string($oid);
+      $offeritems = $this->_database->query("SELECT Status, OfferName FROM `offer` o,`orderitem` i, `order` od WHERE i.fOfferID=o.OfferID AND i.fOrderID=od.OrderID AND od.OrderID = $oid; ");
+      if (!$offeritems)
+          throw new Exception("Query failed:" .$_database->error());
+      $result=[];
+       while($item = $offeritems->fetch_assoc()){
+        array_push($result,new Order($item['Status'],$item['OfferName']));
+      }
+      return $result;// to do: fetch data for this view from the database
+    }
+
+    /**
+     * First the necessary data is fetched and then the HTML is
+     * assembled for output. i.e. the header is generated, the content
+     * of the page ("view") is inserted and -if avaialable- the content of
+     * all views contained is generated.
+     * Finally the footer is added.
+     *
+     * @return none
+     */
+    protected function generateView()
+    {
+
+        $this->generatePageHeader('Kunde');
+        if(isset($_SESSION['oid'])){
+            $items = $this->getViewData($_SESSION['oid']);
+        }else{
+          echo <<<header_no_order
+                <div class="header">
+                  <img src="../res/banner.svg" alt="banner" id="logo">
+                  <div class="header-right">
+                      <a href="bestellung.php">Bestellung</a>
+                      <a class="active" href="#kunde">Kunde</a>
+                      <a href="baecker.php">Bäcker</a>
+                      <a href="fahrer.php">Fahrer</a>
+                  </div>
+                </div>
 
 
-  <body>
+                <div id="main">
+                  <div class="content">
+                    <div class="error">
+                      <div class="img"><img src="../res/top10traurig.png" alt=":("></div>
+                      <div class="msg">Holla, nicht so voreilig, du hast leider noch nichts bestellt. Wenn dein Magen knurrt, kannst du dir <a href="./Bestellung.php">hier</a> unser Angebot ansehen!</div>
+                    </div>
+                  </div>
+                </div>
+
+                <div id="footer">
+                    <ul>
+                        <li><a href="bestellung.php">Bestellung</a></li>
+                        <li><a href="#kunde">Kunde</a></li>
+                        <li><a href="baecker.php">Bäcker</a></li>
+                        <li><a href="fahrer.php">Fahrer</a></li>
+                      </ul>
+                </div>
+
+header_no_order;
+          $this->generatePageFooter();
+          return;
+        }
+
+echo <<<header
+
       <div class="header">
         <img src="../res/banner.svg" alt="banner" id="logo">
         <div class="header-right">
@@ -27,25 +143,24 @@
         </div>
       </div>
 
-
       <div id="main">
         <div class="content">
+header;
+
+    foreach ($items as $item){
+        $ostatus = htmlspecialchars($item->status, ENT_QUOTES | ENT_HTML5 | ENT_DISALLOWED | ENT_SUBSTITUTE, 'UTF-8');
+        $oname = htmlspecialchars($item->name);
+
+echo <<<order
           <div class="order">
-              <div class="Item">Großer Döner</div>
-              <div class="Status">Bestellt</div>
+              <div class="Item">$oname</div>
+              <div class="Status">$ostatus</div>
           </div>
-          <div class="order">
-              <div class="Item">Kleiner Döner</div>
-              <div class="Status">In Zubereitung</div>
-          </div>
-          <div class="order">
-              <div class="Item">Döner Pizza</div>
-              <div class="Status">Fertig</div>
-          </div>
-          <div class="order">
-              <div class="Item">Veget. Döner</div>
-              <div class="Status">In Zustellung</div>
-          </div>
+
+order;
+      }
+
+echo <<<footer
         </div>
       </div>
 
@@ -58,9 +173,59 @@
             </ul>
       </div>
 
-      <!-- <div class="cookiebar open">
-        <div class="left">Zur optimalen Nutzererfahrung nutzen wir auf unserer Website Cookies. 🍪 <a href="datenschutz.html">Mehr Informationen</a></div>
-        <div class="right"><button class="cookiebar-close" onclick="cookieClose()">Verstanden!</button></div>
-      </div> -->
-  </body>
-</html>
+footer;
+        $this->generatePageFooter();
+    }
+
+    /**
+     * Processes the data that comes via GET or POST i.e. CGI.
+     * If this page is supposed to do something with submitted
+     * data do it here.
+     * If the page contains blocks, delegate processing of the
+	 * respective subsets of data to them.
+     *
+     * @return none
+     */
+    protected function processReceivedData()
+    {
+        parent::processReceivedData();
+        // to do: call processReceivedData() for all members
+    }
+
+    /**
+     * This main-function has the only purpose to create an instance
+     * of the class and to get all the things going.
+     * I.e. the operations of the class are called to produce
+     * the output of the HTML-file.
+     * The name "main" is no keyword for php. It is just used to
+     * indicate that function as the central starting point.
+     * To make it simpler this is a static function. That is you can simply
+     * call it without first creating an instance of the class.
+     *
+     * @return none
+     */
+    public static function main()
+    {
+        try {
+            session_start();
+            $page = new Kunde();
+            $page->processReceivedData();
+            $page->generateView();
+        }
+        catch (Exception $e) {
+            header("Content-type: text/plain; charset=UTF-8");
+            echo $e->getMessage();
+        }
+    }
+}
+
+// This call is starting the creation of the page.
+// That is input is processed and output is created.
+Kunde::main();
+
+// Zend standard does not like closing php-tag!
+// PHP doesn't require the closing tag (it is assumed when the file ends).
+// Not specifying the closing ? >  helps to prevent accidents
+// like additional whitespace which will cause session
+// initialization to fail ("headers already sent").
+//? >
